999aecd7fd4ce21c68128d8ba90a8df31b123f2f
Devon Tubben <tubbend1@msu.edu>
2020-11-20 12:00:03 -0500

    commit tree a2db9a8ab3798a6834893d1896d65ea5ccfe0a26

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,17 @@
-require 'pry'
-
-def hello(array)
+def hello_t(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i += 1
+    i = i + 1
   end
+  array
+else 
+  puts "Hey! No block was given!"
+end
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello_t(["Tim", "Tom", "Jim"]) do |name| if name.start_with?("T") 
+  puts "Hi, #{name}" 
+end 
+end 
